332c90483d4c3fa3c48c909ff49456aa8fb8ddc4
pabloigoldi <pablo.emanuel.ig@gmail.com>
2016-10-15 08:08:18 -0300

    ultimo

--- www/js/app.js
+++ www/js/app.js
@@ -3,14 +3,15 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
@@ -22,52 +23,92 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $ionicConfigProvider.tabs.position('bottom');
+  $ionicConfigProvider.scrolling.jsScrolling(true);
   $stateProvider
 
-    .state('app', {
-    url: '/app',
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
     abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+    templateUrl: 'templates/tabs.html'
   })
 
-  .state('app.search', {
-    url: '/search',
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
     views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
       }
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
+
+  .state('tab.inicio', {
+      url: '/inicio',
       views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
+        'tab-Inicio': {
+          templateUrl: 'templates/inicio.html',
+          controller: 'inicioCtrl'
         }
       }
     })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+  .state('tab.autor', {
+      url: '/autor',
+      views: {
+        'tab-autor': {
+          templateUrl: 'templates/tab-autor.html',
+          controller: 'infoCtrl'
+        }
       }
-    }
-  });
+    })
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+  $urlRouterProvider.otherwise('/tab/inicio');
+
+})
+
+
+.directive('onLongPress', function($timeout) {
+  return {
+    restrict: 'A',
+    link: function($scope, $elm, $attrs) {
+      $elm.bind('touchstart', function(evt) {
+        // Locally scoped variable that will keep track of the long press
+        $scope.longPress = true;
+
+        // We'll set a timeout for 600 ms for a long press
+        $timeout(function() {
+          if ($scope.longPress) {
+            // If the touchend event hasn't fired,
+            // apply the function given in on the element's on-long-press attribute
+            $scope.$apply(function() {
+              $scope.$eval($attrs.onLongPress)
+            });
+          }
+        }, 600);
+      });
+
+      $elm.bind('touchend', function(evt) {
+        // Prevent the onLongPress event from firing
+        $scope.longPress = false;
+        // If there is an on-touch-end function attached to this element, apply it
+        if ($attrs.onTouchEnd) {
+          $scope.$apply(function() {
+            $scope.$eval($attrs.onTouchEnd)
+          });
+        }
+      });
+    }
+  };
+})
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,279 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('DashCtrl', function($scope, $ionicPlatform, $cordovaMedia, $timeout, $cordovaDeviceMotion) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  $scope.arrayPlay = {Izquierda: 1, Derecha: 1, Abajo: 1, Arriba: 1, Acostado: 1};
 
-  // Form data for the login modal
-  $scope.loginData = {};
+ $scope.Ingresar = false;
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+ $scope.Empezar = function(){
+   $scope.Ingresar = true;
+ }
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+  $ionicPlatform.ready(function() {
+    
+    var options = { frequency: 1000 };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+        var watch = $cordovaDeviceMotion.watchAcceleration(options);
+        watch.then(
+          null,
+          function(error) {
+          console.log("No se puede tomar coordenadas desde ordenador");
+          },
+          function(result) {
+            $scope.X = parseInt(result.x);
+            $scope.Y = parseInt(result.y);
+            $scope.Z = parseInt(result.z);
+            $scope.tiempo = result.timestamp;
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+            if($scope.X > 4 && $scope.audioIzq != null && $scope.arrayPlay["Izquierda"] == 1){
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+              $scope.audioIzq.play();
+              $scope.arrayPlay = {Izquierda: 0, Derecha: 1, Abajo: 1, Arriba: 1, Acostado: 1};
+
+            }
+
+            if($scope.X < -4 && $scope.audioDer != null && $scope.arrayPlay["Derecha"] == 1){
+
+              $scope.audioDer.play();
+              $scope.arrayPlay = {Izquierda: 1, Derecha: 0, Abajo: 1, Arriba: 1, Acostado: 1};
+
+            }
+
+            if($scope.Y > 5 && $scope.audioArriba != null && $scope.arrayPlay["Arriba"] == 1){
+
+              $scope.audioArriba.play();
+              $scope.arrayPlay = {Izquierda: 1, Derecha: 1, Abajo: 1, Arriba: 0, Acostado: 1};
+
+            }
+
+            if($scope.Z > 8 && $scope.audioAcostado != null && $scope.arrayPlay["Acostado"] == 1){
+
+              $scope.audioAcostado.play();
+              $scope.arrayPlay = {Izquierda: 1, Derecha: 1, Abajo: 1, Arriba: 1, Acostado: 0};
+
+            }
+
+            if($scope.Z < -7 && $scope.audioAbajo != null && $scope.arrayPlay["Abajo"] == 1){
+
+              $scope.audioAbajo.play();
+              $scope.arrayPlay = {Izquierda: 1, Derecha: 1, Abajo: 0, Arriba: 1, Acostado: 1};
+
+            }
+
+
+        });
+
+    })
+
+
+
+  $scope.AudioIzquierda = function(){
+
+    $ionicPlatform.ready(function() {
+
+      try{
+        var src = "izquierda.mp3";
+        $scope.audioIzq = $cordovaMedia.newMedia(src);
+        $scope.audioIzq.startRecord();
+        //var detalles = JSON.stringify($scope.audioIzq);
+        //alert(detalles);
+
+        /*$timeout(function(){
+          $scope.audioIzq.stopRecord();
+          alert("grabado!!!");
+        }, 2000)*/
+
+      } catch(ex){
+
+        alert(ex);
+      }
+      
+    });
+
+  }
+
+  $scope.Detener = function(btn){
+    try{
+
+      if(btn == 'btnIzquierda'){
+
+        $scope.audioIzq.stopRecord();
+
+      }
+
+      if(btn == 'btnDerecha'){
+
+        $scope.audioDer.stopRecord();
+
+      }
+
+      if(btn == 'btnAbajo'){
+
+        $scope.audioAbajo.stopRecord();
+
+      }
+
+      if(btn == 'btnArriba'){
+
+        $scope.audioArriba.stopRecord();
+
+      }
+
+      if(btn == 'btnAcostado'){
+
+        $scope.audioAcostado.stopRecord();
+
+      }
+      
+
+    } catch(ex){
+      alert(ex);
+    }
+  }
+
+  $scope.AudioDerecha = function(){
+    
+    $ionicPlatform.ready(function() {
+
+      try{
+        var src = "derecha.mp3";
+        $scope.audioDer = $cordovaMedia.newMedia(src);
+        $scope.audioDer.startRecord();
+
+      } catch(ex){
+
+        alert(ex);
+      }
+      
+    });
+
+  }
+
+
+
+  $scope.AudioAbajo = function(){
+    
+    $ionicPlatform.ready(function() {
+
+      try{
+        var src = "abajo.mp3";
+        $scope.audioAbajo = $cordovaMedia.newMedia(src);
+        $scope.audioAbajo.startRecord();
+
+      } catch(ex){
+
+        alert(ex);
+      }
+      
+    });
+
+  }
+
+
+  $scope.AudioArriba = function(){
+    
+    $ionicPlatform.ready(function() {
+
+      try{
+        var src = "arriba.mp3";
+        $scope.audioArriba = $cordovaMedia.newMedia(src);
+        $scope.audioArriba.startRecord();
+
+      } catch(ex){
+
+        alert(ex);
+      }
+      
+    });
+
+  }
+
+  $scope.AudioAcostado = function(){
+    
+    $ionicPlatform.ready(function() {
+
+      try{
+        var src = "acostado.mp3";
+        $scope.audioAcostado = $cordovaMedia.newMedia(src);
+        $scope.audioAcostado.startRecord();
+
+      } catch(ex){
+
+        alert(ex);
+      }
+      
+    });
+
+  }
+
+
+
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('inicioCtrl', function($scope, $state) {
+    $scope.loginData = {};
+  $scope.loginData.username ="pablo.emanuel.ig@gmail.com";
+  $scope.loginData.password = "hola06";
+
+  $scope.doLogin= function()
+  {
+      firebase.auth().signInWithEmailAndPassword($scope.loginData.username,$scope.loginData.password).catch(function(error)
+        {          
+          console.info("error: " + error);
+           var errorCode = error.code;
+           var errorMessage = error.message;
+          if(errorCode== 'auth/wrong-password')
+          {
+            $scope.mensajeLogin = "wrong-password";
+          }
+          else
+          {
+          $scope.mensajeLogin = "errores:" + errorMessage;
+          }
+
+
+        }).then(function(respuesta){
+
+              if(respuesta)
+              {             
+                  $state.go('tab.dash');
+              }
+              else
+              {
+                   $scope.mensajeLogin = "Error en el logueo";
+              }
+
+        });
+  }
+
+  $scope.salir= function()
+  {
+      ionic.Platform.exitApp();
+  }
+
+ }) 
+
+.controller('infoCtrl', function($scope, $ionicPlatform, $cordovaInAppBrowser){
+
+
+    var options = {
+      location: 'yes',
+      clearcache: 'yes',
+      toolbar: 'no'
+    };
+
+    $scope.showBrowser=function(){
+
+      $ionicPlatform.ready(function(){
+
+        $cordovaInAppBrowser.open('https://github.com/pabloigoldi/TPMovimientosIonic2016', '_system', options);
+
+      });
+    };
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+.controller('AutorCtrl', function($scope, $ionicPlatform, $cordovaInAppBrowser) {
+
+
+})
